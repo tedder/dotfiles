041447f0c91bf2533eadfef42a0452fc43315683
Ted Timmons <ted@perljam.net>
2014-10-14 15:47:32 -0700

    incrementals

--- bin/pull-torrents.sh
+++ bin/pull-torrents.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-rsync --exclude "Various*" --exclude "Raven*" --exclude "NWoBHM*" --exclude "saxon*" --exclude "*meta" --checksum --partial --progress --recursive seedbox2:/opt/torrent-files/downloads/*  ~/Downloads/
+if [ ! -d /Volumes/sda2 ]; then
+  echo "mounting dir"
+  mount_smbfs //@192.168.1.1/sda2 /Volumes/sda2
+fi
 
+rsync --exclude "Various*" --exclude "Raven*" --exclude "NWoBHM*" --exclude "saxon*" --exclude "*meta" --checksum --partial --progress --recursive seedbox2:/opt/torrent-files/downloads/*  /Volumes/sda2/vids/
