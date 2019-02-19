995784f6a87ef64949d63a7978021b15921b2c25
ted <ted@signalsciences.com>
2019-02-18 17:11:03 -0700

    allow the pw to be set by  variable

--- bin/mqtt.sh
+++ bin/mqtt.sh
@@ -2,4 +2,5 @@ mosquitto_sub -h 192.168.0.23 -P $mqtt_password \
  -u octo -v -t '#' \
  -T 'octoprint/e10/temperature/#' -T 'octoprint/e10/event/CaptureDone' -T 'octoprint/e10/event/CaptureStart' \
  -T 'octoprint/prusamk3/temperature/#' -T 'octoprint/prusamk3/event/CaptureDone' -T 'octoprint/prusamk3/event/CaptureStart' -T 'octoprint/prusamk3/event/ZChange' \
- -T 'octoprint/mpdelta/temperature/#' -T 'octoprint/mpdelta/event/CaptureDone' -T 'octoprint/mpdelta/event/CaptureStart' -T 'octoprint/mpdelta/event/ZChange'
+ -T 'octoprint/mpdelta/temperature/#' -T 'octoprint/mpdelta/event/CaptureDone' -T 'octoprint/mpdelta/event/CaptureStart' -T 'octoprint/mpdelta/event/ZChange' \
+ -T 'iot/#'
